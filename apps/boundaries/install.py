322804aaf2e93a72208602db623a476633d8c5cb
pauljako <paulifey18@gmail.com>
2024-04-01 22:09:01 +0200

    Added

---
+++ apps/boundaries/install.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+import json
+import os.path
+import pathlib
+
+with open("boundaries.json", "rb") as f:
+    info = json.load(f)
+print("boundaries - Version: " + info["version"])
+
+os.system("chmod +x main.py")
+
+print("Checking Folder Structure...")
+os.chdir("../..")
+
+if not os.path.exists("var"):
+    print("Warning: var dir does not exist. Creating it...")
+    pathlib.Path("var").mkdir()
+    pathlib.Path("var/boundaries").mkdir()
+
+if not os.path.exists("exec"):
+    print("Warning: exec dir does not exist. Creating it...")
+    pathlib.Path("exec").mkdir()
+    pathlib.Path("exec/bin").mkdir()
+    Path("exec/desktop").symlink_to(os.path.realpath(os.path.expanduser("~/.local/share/applications")))
+
+if not os.path.exists("exec/bin"):
+    print("Warning: exec/bin dir does not exist. Creating it...")
+    pathlib.Path("exec/bin").mkdir()
+
+if not os.path.exists("exec/desktop"):
+    print("Warning: exec/desktop dir does not exist. Making a Symlink...")
+    Path("exec/desktop").symlink_to(os.path.realpath(os.path.expanduser("~/.local/share/applications")))
+
+if not os.path.exists("bin"):
+    print("Warning: bin dir does not exist. Making a Symlink...")
+    Path("bin").symlink_to("apps/boundaries")
+
+print("Folder Structure checking done")
